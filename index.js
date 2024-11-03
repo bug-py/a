946f37e8258f09c7462f4844193d1b284e07683c
bug-py <cocorigolo6@gmail.com>
2024-11-03 18:02:11 +0100

    init

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const express=require("express")
 const app=express()
 const http=require("http").createServer(app)
 const io=new require("socket.io")(http)
+let user=0;
 app.use("/public",express.static(__dirname+"/assets"))
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html")
     
 })
 io.on("connection",socket=>{
+    user++
+    nom=user
     console.log("ici")
     socket.on("message",json=>{
         io.emit("message",{
          ms:Date.now()-json.date,
          message:json.message,
-         name:"ok",
+         name:`Invité ${user}`,
          date:json.date
         })
     })
     socket.on("diconnect",()=>{
+     user--
      console.log("parti")
     })
 })
